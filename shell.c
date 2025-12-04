828e5ceb33d9543f49187d5382303d57f669e1f2
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2025-12-04 11:32:37 +0100

    ajout du code du tp 4 de progSys

---
+++ shell.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "./ligne-commande/ligne_commande.h"
+#include <sys/wait.h>
+
+
+#define MAX_HOSTNAME 256
+#define MAX_PATH 256
+
+// Affiche un prompt à l'utilisateur
+void affiche_prompt();
+
+// Lit une ligne depuis l'entrée standard et la traite de façon à
+// produire une liste de commandes à exécuter.
+//
+// Une liste de commande est un tableau de commande.  Une commande et
+// une table de chaîne de caractères dans une formation compatible avec
+// le tableau argv d'un main.
+//
+// l'entier pointé par flag sera mis à jour avec une des valeur suivantes:
+// - -1 : une erreur s'est produite
+// - 0 : la ligne a été analysée correctement et elle ne se termine pas par le caractère &
+// - 1 : la ligne a été analysée correctement et elle se termine par le caractère &
+//
+// l'entier pointé par nb sera mis à jour avec le nombre de
+// commandes retournées, c'est à dire la taille du tableau de commandes
+//
+// En cas d'erreur, la fonction retourne NULL
+//char ***ligne_commande(int *flag, int *nb);
+
+void execute_ligne_commande(char ***ligneCommande, int nb, int flag);
+
+int main(void) {
+
+    while(1) {
+        affiche_prompt();
+        int flag = 0;
+        int nb = 0;
+        char ***ligneCommande = ligne_commande(&flag, &nb);
+        if(ligneCommande == NULL || strcmp(ligneCommande[0][0], "exit") == 0 ) {
+            break;
+        } else if (flag != -1) {
+            execute_ligne_commande(ligneCommande, nb, flag);
+        }
+    }
+    printf("\n");
+}
+
+void affiche_prompt() {
+    char hostname[MAX_HOSTNAME];
+    char cwd[MAX_PATH];
+    char *username;
+    
+    // Récupérer le nom d'utilisateur
+    username = getenv("USER");
+    if (username == NULL) {
+        username = "unknown";
+    }
+    // Récupérer le nom de la machine
+    if (gethostname(hostname, MAX_HOSTNAME) != 0) {
+        strcpy(hostname, "unknown");
+    }
+    // Récupérer le répertoire courant
+    if (getcwd(cwd, MAX_PATH) == NULL) {
+        strcpy(cwd, "unknown");
+    }
+    
+    printf("%s@%s:%s> ", username, hostname, strrchr(cwd, '/'));
+    fflush(stdout);
+}
+
+void execute_ligne_commande(char ***ligneCommande, int nb, int flag) {
+    
+    for (int i = 0; i < nb; i++) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            return;
+        }
+        else if (pid == 0) {
+            // Exécuter la commande si réussi alors break
+            execvp(ligneCommande[i][0], ligneCommande[i]);
+
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+        else {
+            int status;
+            if (flag == 0) {
+                waitpid(pid, &status, 0);
+            }
+        }
+    }
+}
+
